a7d4c7d35cae06f781a3d089c3c1b4d2bda4b1c4
Brad Wilson <dotnetguy@gmail.com>
2015-01-18 11:23:24 -0800

    Put back the cache buster for ASP.NET builds

--- build.ps1
+++ build.ps1
@@ -1,13 +1,23 @@
+# Have to bust the cache, because of broken packages from the CLR team
+remove-item -recurse -force $(join-path $env:USERPROFILE ".kpm\packages") -ErrorAction SilentlyContinue
+
+# Make sure beta 2 is installed and in use
 & tools\kvm install 1.0.0-beta2 -runtime CLR -x86
 & tools\kvm install 1.0.0-beta2 -runtime CoreCLR -x86
 & tools\kvm use 1.0.0-beta2 -runtime CLR -x86
-& kpm restore
 
+# Update build number during CI
 if ($env:BuildSemanticVersion -ne $null) {
   $content = get-content src\xunit.runner.aspnet\project.json
   $content = $content.Replace("99.99.99", $env:BuildSemanticVersion)
   set-content src\xunit.runner.aspnet\project.json $content -encoding UTF8
 }
 
+# Restore packages and build
+& kpm restore
 & kpm build src\xunit.runner.aspnet --configuration Release
-pushd test\test.xunit.runner.aspnet; & k test; popd
+
+# Run tests
+push-location test\test.xunit.runner.aspnet
+& k test
+pop-location
