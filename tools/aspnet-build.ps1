241970a1499c455f4b9c4c4a0f950af5cb3979ef
Brad Wilson <dotnetguy@gmail.com>
2015-04-01 18:09:00 -0700

    Update to use latest for Windows builds

--- tools/aspnet-build.ps1
+++ tools/aspnet-build.ps1
@@ -1,9 +1,9 @@
 # Have to bust the cache, because of broken packages from the CLR team
-remove-item -recurse -force $(join-path $env:USERPROFILE ".kpm\packages") -ErrorAction SilentlyContinue
+remove-item -recurse -force $(join-path $env:USERPROFILE ".dnx\packages") -ErrorAction SilentlyContinue
 
-# Make sure beta 3 is installed and in use
-& tools\kvm install 1.0.0-beta3 -runtime CoreCLR -x86
-& tools\kvm install 1.0.0-beta3 -runtime CLR -x86
+# Make sure latest is installed and in use
+& tools\dnvm install latest -runtime CoreCLR -arch x86
+& tools\dnvm install latest -runtime CLR -arch x86
 
 # Update build number during CI
 if ($env:BuildSemanticVersion -ne $null) {
@@ -13,5 +13,5 @@ if ($env:BuildSemanticVersion -ne $null) {
 }
 
 # Restore packages and build
-& kpm restore
-& kpm pack src\xunit.runner.aspnet --configuration Release
+& dnu restore
+& dnu pack src\xunit.runner.aspnet --configuration Release
